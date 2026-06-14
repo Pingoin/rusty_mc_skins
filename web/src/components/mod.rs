54d6284be696fe3d5de5163d939fef3245f12cc7
Pingoin <pdrente@googlemail.com>
2026-06-14 14:37:28 +0200

    feat: texture creation dialog

--- web/src/components/mod.rs
+++ web/src/components/mod.rs
@@ -1,5 +1,3 @@
 mod navbar;
-mod new_texture;
 pub use navbar::Navbar;
 pub use navbar::NavItems;
-pub use new_texture::NewTexture;
--- web/src/views/texture_list.rs
+++ web/src/views/texture_list.rs
@@ -1,12 +1,11 @@
-use api::{Permissions, Texture, TextureType, User, get_me, get_textures, set_texture};
+use api::{Blob, Permissions, Texture, TextureType, get_textures, set_texture};
 use dioxus::prelude::*;
 
-
-use crate::{components::NewTexture, has_permission};
+use crate::{USER, has_permission, reload_me};
 
 #[component]
 pub fn TextureList(tex_type: TextureType) -> Element {
-    let textures = use_resource(use_reactive!(|tex_type| async move {
+    let mut textures = use_resource(use_reactive!(|tex_type| async move {
         get_textures()
             .await
             .unwrap_or_default()
@@ -14,15 +13,19 @@ pub fn TextureList(tex_type: TextureType) -> Element {
             .filter(|t| tex_type == t.texture_type)
             .collect::<Vec<Texture>>()
     }));
-    let user = use_resource(|| async move { get_me().await.ok() });
 
+        let mut texture = use_signal(|| {
+        let mut tex = Texture::default();
+        tex.texture_type = tex_type;
+        tex
+    });
     rsx! {
 
         article { class: "",
             h1 { "Texture List" }
             div { class: "columns-2 gap-4 sm:columns-3 sm:gap-8",
                 for texture in textures.cloned().unwrap_or_default() {
-                    TextureCard { texture, user_res: user.clone() }
+                    TextureCard { texture }
                 }
             }
             if has_permission(Permissions::TEXTURE_EDIT) {
@@ -35,30 +38,80 @@ pub fn TextureList(tex_type: TextureType) -> Element {
                 }
                 dialog { class: "modal", id: "my_modal_1",
                     div { class: "modal-box",
-                        h3 { class: "text-lg font-bold", "Hello!" }
-                        p { class: "py-4", "Press ESC key or click the button below to close" }
                         div { class: "modal-action",
-                            NewTexture { tex_type }
+                            form {
+                                onsubmit: move |evt| {
+                                    evt.prevent_default();
+                                    spawn(async move {
+                                        let t = texture.read().clone();
+                                        api::create_texture(t).await.unwrap();
+                                        let _ = document::eval("document.getElementById('my_modal_1').close()");
+                                        textures.restart();
+                                    });
+                                },
+
+                                h1 { "Texture" }
+                                fieldset { class: "fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4",
+                                    label { class: "label",
+                                        "Texture Name"
+                                        input {
+                                            class: "input",
+                                            placeholder: "Texture Name",
+                                            r#type: "text",
+                                            value: "{texture.read().skin_name}",
+                                            oninput: move |e| {
+                                                let mut t = texture.read().clone();
+                                                t.skin_name = e.value().clone();
+                                                texture.set(t);
+                                            },
+                                        }
+                                    }
+                                    input {
+                                        r#type: "file",
+                                        class: "file-input file-input-primary",
+                                        accept: "image/png",
+                                        onchange: move |evt| {
+                                            async move {
+                                                for file in evt.files() {
+                                                    if let Ok(file) = file.read_bytes().await {
+                                                        let data = Blob(file.into());
+                                                        let mut t = texture.read().clone();
+                                                        t.image_data = data;
+                                                        let _ = t.compress();
+                                                        texture.set(t);
+                                                    }
+                                                }
+                                            }
+                                        },
+                                    }
+                                    if !texture.read().image_data.is_empty() {
+                                        img {
+                                            src: "data:image/png;base64,{texture.read().get_preview().unwrap_or_default().as_base64()}",
+                                            width: "100",
+                                        }
+                                    }
+                                    button {
+                                        class: "btn btn-primary",
+                                        r#type: "submit",
+                                        "Save"
+                                    }
+                                }
+                            }
                         }
                     }
                 }
             }
         
         }
-    } 
+    }
 }
 
 #[component]
-fn TextureCard(texture: Texture, user_res: Resource<Option<User>>) -> Element {
-    let user = user_res.cloned().flatten();
-
-    let is_set = match user {
-        Some(user) => match texture.texture_type {
-            TextureType::Skin => user.selected_skin_id == Some(texture.id.clone()),
-            TextureType::Cape => user.selected_cape_id == Some(texture.id.clone()),
-            TextureType::Elytra => user.selected_elytra_id == Some(texture.id.clone()),
-        },
-        None => false,
+fn TextureCard(texture: Texture) -> Element {
+    let is_set = match texture.texture_type {
+        TextureType::Skin => USER.cloned().selected_skin_id == Some(texture.id.clone()),
+        TextureType::Cape => USER.cloned().selected_cape_id == Some(texture.id.clone()),
+        TextureType::Elytra => USER.cloned().selected_elytra_id == Some(texture.id.clone()),
     };
 
     let set_me = set_texture;
@@ -90,7 +143,7 @@ fn TextureCard(texture: Texture, user_res: Resource<Option<User>>) -> Element {
                             class: "btn btn-primary",
                             onclick: move |_| async move {
                                 let _ = set_me(me()).await;
-                                user_res.set(Some(get_me().await.ok()));
+                                reload_me();
                             },
                             "Apply to me"
                         }
